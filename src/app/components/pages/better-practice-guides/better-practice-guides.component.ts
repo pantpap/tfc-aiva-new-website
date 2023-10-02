fca0d76e9419e5dced6404776470f47630a1dde5
PP <pantpap@gmail.com>
2023-10-02 10:21:48 +0300

    create better-practice-guides.component page

---
+++ src/app/components/pages/better-practice-guides/better-practice-guides.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-better-practice-guides',
+  templateUrl: './better-practice-guides.component.html',
+  styleUrls: ['./better-practice-guides.component.scss']
+})
+export class BetterPracticeGuidesComponent {
+
+}
