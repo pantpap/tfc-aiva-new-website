2b10bbf84af52c7b0e7d46f6c6c8f096cbee7a85
PA <pantpap@gmail.com>
2023-10-08 00:48:00 +0300

    display current year programmatically on footer

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+    year!: number;
   constructor() { }
 
   ngOnInit(): void {
+      this.year = this.findCurrentYear();
   }
 
+  private findCurrentYear() {
+      const date = new Date();
+      return date.getFullYear();
+  }
 }
