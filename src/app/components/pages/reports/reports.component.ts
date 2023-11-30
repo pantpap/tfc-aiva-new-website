d9c2772f3a6ee925dd4aec3255fe7ca20172af26
Pantelis Papasavvas <pantpap@gmail.com>
2023-11-30 17:56:42 +0200

    added banner to reports

--- src/app/components/pages/reports/reports.component.ts
+++ src/app/components/pages/reports/reports.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./reports.component.scss']
 })
 export class ReportsComponent {
-
+  //  TODO create an array to hold all these data like projects for example
+  // TODO reverse the display order. Latest added should be firts
 }
