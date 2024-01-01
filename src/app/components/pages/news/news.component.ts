5e06e340f2cab5d2c3db82b4bb4fabf0b6d380d9
Pantelis Papasavvas <pantpap@gmail.com>
2024-01-01 22:22:49 +0200

    rendering news from file and reverse order

--- src/app/components/pages/news/news.component.ts
+++ src/app/components/pages/news/news.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import { news } from 'src/app/data/news';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.scss']
+  styleUrls: ['./news.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class NewsComponent {
-
+    readonly news = news;
 }
---
+++ src/app/data/news.ts
@@ -0,0 +1,132 @@
+interface New {
+    id: number;
+    date: string;
+    title: string;
+    shortDescription: string;
+    hasExternalURL: boolean;
+    url: string;
+}
+export const news: New[] = [
+    {
+        id: 1,
+        date: 'July 2023',
+        title: 'EUROPOL and TFC, Den Haag',
+        shortDescription: `<p>
+                                Based on the work undertaken in No-Fear Project, partner TFC Research & Innovation
+                                Limited recently presented their work to Europol on Standardisation as well as on the
+                                recently established Horizon Europe PEERS project.
+                            </p>`,
+        hasExternalURL: false,
+        url: '/news/europol',
+    },
+    {
+        id: 2,
+        date: 'August 2023',
+        title: 'CS_AIW Cluster and the EU efforts to fight Cancer',
+        shortDescription: `<p>
+                                The Commission has set an overall goal of improving the lives of more than 3 million
+                                people by 2030. The Cancer Survivorship – AI for Well-being cluster is helping for the
+                                realization of this objective to be realized. The cluster brings together 11 EU-funded
+                                projects working in Artificial Intelligence for healthcare and well-being. Primarily,
+                                the Cluster focuses in the post-cancer treatment space with a collective aim to
+                                cross-fertilise. This brings to transcend individual project experiences for the wider
+                                good and outcome of their individual projects. The Cluster started in October 2020 and
+                                it is evolving. Presently, it is flourishing with members collectively working on
+                                related issues spanning topics around clinical, technical and business. The Cluster
+                                fosters cooperation for the ultimate benefit of the patients, care-givers and of course,
+                                medical professionals involved in the care team undertaking. The News article explains how
+                                the work of the Cluster is helping the European Commission to reach is
+                            </p>`,
+        hasExternalURL: true,
+        url: 'https://www.h2020-faith.eu/2023/08/cs_aiw-cluster-and-the-eu-efforts-to-fight-cancer/',
+    },
+    {
+        id: 3,
+        date: 'September 2023',
+        title: `TFC participating at the PEERS
+                online workshop September 2023`,
+        shortDescription: `<p>
+                                TFC presenting to Policy Makers, Standardisation Experts and Practitioners at the PEERS
+                                'Look, Listen, Learn, Observe and Act' Online workshop are Tom Flynn and Prof. David Crouch.
+                                The workshop brings the alpha-platform to the target community over 3 streams so that the
+                                co-creation engagement process continues to enable needed capability by frontline
+                                responders to be better captured in a disaster risk management and CBRN-E situation,
+                                specifically in the preparedness and response.
+                                Read about David's involvement in the Emergency Live journal
+                            </p>`,
+        hasExternalURL: true,
+        url: 'https://www.emergency-live.com/marketplace/david-crouch-will-discuss-the-latest-trends-in-cbrne/',
+    },
+    {
+        id: 4,
+        date: 'September 2023',
+        title: `Linking in with related Projects - INCLUDING`,
+        shortDescription: `<p>
+                                How does the PEERS project engage with INCLUDING? Have a read of the article published
+                                in the INCLUDING project. Interoperability is a vital part of the PEERS project and also
+                                for INCLUDING. Both the INCLUDING and PEERS projects are inter-related by their mission
+                                and quest to strengthen European resilience, although the scope of PEERS is wider as it
+                                covers the full spectrum of CBRNe and not only the nuclear and radiological threat aspects.
+                                Notwithstanding that, Article 6 of the European Commission Action Plan between Civil,
+                                Space and Defense Industry, 2021 is extremely prevalent to both projects and in mutual
+                                support the development of Better Practice Guides and APIs to strengthen interoperability,
+                                will further help to contribute to the sharing of knowledge and resources as well as the
+                                long-term sustainability intent directed by the Co-Creation Group in PEERS. Through the
+                                integrated Virtual Reality component of the PEERS ecosystem, the learning capacity of
+                                INCLUDING can be strengthened for the wider benefit of the community supported by
+                                both projects. PEERS is committed to holding more Meetings of Minds (MoM) events and
+                                sees a logical connection in obtaining further co-creation feedback so that the gamma
+                                version of the PEERS platform delivers what the community needs and complements the
+                                federated model of INCLUDING.
+                            </p>`,
+        hasExternalURL: true,
+        url: 'https://www.linkedin.com/posts/h2020including_including-newsletter-n8-activity-7107713930327695366-0nYP/?utm_source=share&utm_medium=member_desktop',
+    },
+    {
+        id: 5,
+        date: 'November 2023',
+        title: `Join PEERS Co-Creation Group`,
+        shortDescription: `<p>
+                                Practitioners and Policy makers, get involved by joining us in the Co-creation Group to
+                                help us delivery the  platform that is needed by the CBRN-E community we serve. <br>
+                                (<a href="https://tinyurl.com/peers-request" target="_blank">https://tinyurl.com/peers-request</a>)
+                            </p>
+                            <p>
+                                <img src="../../assets/images/QRCodes.png"/>
+                            </p>`,
+        hasExternalURL: false,
+        url: '/news/join-peers-group',
+    },
+    {
+        id: 6,
+        date: 'November 2023',
+        title: `TFC presenting at the joint NSAI and PEERS project webinar`,
+        shortDescription: `<p>
+                               Meet the speakers of our webinar with the National Standards Authority of Ireland on 12/12
+                            </p>`,
+        hasExternalURL: true,
+        url: 'https://www.linkedin.com/feed/update/urn:li:activity:7133025433112965120',
+    },
+    {
+        id: 7,
+        date: 'November 2023',
+        title: `PEERS And NSAI Webinar To Strengthen CBRN-E Security In Europe`,
+        shortDescription: `<p>
+                               The National Standards Authority of Ireland (NSAI) and the PracticE Ecosystem for StandaRdS
+                                (PEERS) project are collaboratively hosting a webinar on December 12, 2023, titled 'Enhancing CBRN-E Preparedness and Response Capabilities through Standardisation'.
+                            </p>`,
+        hasExternalURL: true,
+        url: 'https://www.emergency-live.com/news/peers-and-nsai-webinar-to-strengthen-cbrn-e-security-in-europe/',
+    },
+    {
+        id: 8,
+        date: 'November 2023',
+        title: `B-Prepared Citizen -center disaster preparedness`,
+        shortDescription: `<p>
+                               In the wake of recent catastrophic events such as the 2021 floods in Germany, B-PREPARED emerges as a beacon of hope in the quest to reduce
+                                the devastating impact of disasters.
+                            </p>`,
+        hasExternalURL: true,
+        url: 'https://docs.google.com/document/d/1G9Fav7WOF09474SiOkaVliOrmNDNvqsb7f_y5hFuw-c/edit',
+    }
+];
